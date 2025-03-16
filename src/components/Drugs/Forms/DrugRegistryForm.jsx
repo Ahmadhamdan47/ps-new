7f0042283a1e951ce3cbb0a93d8c42eadc5e6cbf
Ahmadhamdan47 <ahmadhamdan47@gmail.com>
2025-03-16 13:05:47 +0200

    fix drug name

--- src/components/Drugs/Forms/DrugRegistryForm.jsx
+++ src/components/Drugs/Forms/DrugRegistryForm.jsx
@@ -215,6 +215,8 @@ const DrugRegistryForm = () => {
             name="DrugName"
             value={formData.DrugName}
             onChange={(e) => handleInputChange(e)}
+            className="mt-1 w-full rounded-full text-lg border border-[#00a65100] dark:border-black-border bg-white-bg dark:bg-black-input px-4 py-2 font-normal shadow-md dark:shadow-black-shadow outline-none focus:border-green-pri focus:outline-none focus:ring-2 focus:ring-green-pri dark:focus:ring-2 dark:focus:ring-green-pri"
+
             type="text"
             autoComplete="off"
             placeholder="name"
